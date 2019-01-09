b944b14637b86dc42ceaef54fa5397de3fb40615
Max Fierro <elmasse@gmail.com>
2019-01-09 11:00:22 -0300

    Fix issue with success message as key. Refactor find changelog. Add missing semicolons.

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-const getSummary = {
-  success: ':+1: Great! A changelog file has been found and it has been updated',
-  neutral: ':warning: Oh oh. We could not found any changelog file nor has been updated.'
+
+const CHANGELOG = 'changelog.md';
+
+const VALID_STATUS = ['added', 'modified'];
+
+const SUCCESS = 'success';
+const NEUTRAL = 'neutral';
+
+const SUMMARY = {
+  [SUCCESS]: ':+1: Great! A changelog file has been found and it has been updated',
+  [NEUTRAL]: ':warning: Oh oh. We could not found any changelog file nor has been updated.'
 };
 
-const getText = {
-  success: 'No further action is required.',
-  neutral: 'You can ask the contributor to run `chan [TYPE-OF-CHANGE] [message]`'
-}
+const MESSAGE = {
+  [SUCCESS]: 'No further action is required.',
+  [NEUTRAL]: 'You can ask the contributor to run `chan [TYPE-OF-CHANGE] [message]`'
+};
 
 // Checks API example
 // See: https://developer.github.com/v3/checks/ to learn more
 module.exports = app => {
-  app.on(['check_suite.requested', 'check_run.rerequested'], check)
+  app.on(['check_suite.requested', 'check_run.rerequested'], check);
 
   async function check (context) {
     // Do stuff
-    const { head_branch, head_sha, head_commit } = context.payload.check_suite
-    const { name, owner } = context.payload.repository
+    const { head_branch, head_sha, head_commit } = context.payload.check_suite;
+    const { name, owner } = context.payload.repository;
     // Probot API note: context.repo() => {username: 'hiimbex', repo: 'testing-things'}
-    context.log('>>>>>> CHECK SUITE / CHECK RUN ')
+    context.log('>>>>>> CHECK SUITE / CHECK RUN ');
     if (!head_commit) return;
     let commit;
     try {
@@ -26,24 +34,15 @@ module.exports = app => {
         owner: owner.login,
         repo: name,
         sha: head_commit.id
-      })
+      });
     }
     catch (err) {
       context.log({ err });
       return;
     }
     const { files } = commit.data;
-    let found = false;
-    for (let file of files) {
-      const validName = file.filename.toLowerCase() === 'changelog.md';
-      const validStatus = file.status === 'added' || file.status === 'modified';
-      if (validName && validStatus) {
-        found = true;
-        break;
-      };
-    }
-
-    const result = found ? 'success √' : 'neutral';
+    const found = files.find(({filename, status}) => (filename.toLowerCase() === CHANGELOG && (VALID_STATUS.includes(status))));
+    const result = found ? SUCCESS : NEUTRAL;
 
     return context.github.checks.create(context.repo({
       name: 'Chaas by GEUT',
@@ -54,14 +53,14 @@ module.exports = app => {
       completed_at: new Date(),
       output: {
         title: 'Chaas check',
-        summary: getSummary[result],
-        text: getText[result]
+        summary: SUMMARY[result],
+        text: MESSAGE[result]
       }
-    }))
+    }));
   }
 
   async function pr (context) {
-    context.log('>>>>>> PULL REQUEST ')
-    context.log({ payload: context.payload })
+    context.log('>>>>>> PULL REQUEST ');
+    context.log({ payload: context.payload });
   }
 }
