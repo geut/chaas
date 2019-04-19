b7d25afa4b3ca94a5cdf2c596f9584c3eb6f070b
Max Fierro <elmasse@gmail.com>
2019-04-19 17:15:49 -0300

    Added: Make check fail if no changelog is present.

--- src/check.js
+++ src/check.js
@@ -7,15 +7,18 @@ const VALID_STATUS = ['added', 'modified']
 
 const SUCCESS = 'success'
 const NEUTRAL = 'neutral'
+const FAILED = 'failed'
 
 const SUMMARY = {
-  [SUCCESS]: ':+1: Great! A changelog file has been found and it has been updated',
-  [NEUTRAL]: ':warning: Oh oh. We could not found any changelog file nor has been updated.'
+  [SUCCESS]: ':+1: Great! A CHANGELOG file has been found and it has been updated',
+  [NEUTRAL]: 'Your PR only contains ignored files. No CHANGELOG change required.',
+  [FAILED]: ':warning: Oh oh. We could not found any updated CHANGELOG entries.'
 }
 
 const MESSAGE = {
   [SUCCESS]: 'No further action is required.',
-  [NEUTRAL]: 'You can ask the contributor to run `chan [TYPE-OF-CHANGE] [message]`'
+  [NEUTRAL]: 'No further action is required.',
+  [FAILED]: 'Modify your CHANGELOG file accordingly to introduced changes.'
 }
 
 async function check (context) {
@@ -39,9 +42,6 @@ async function check (context) {
     return
   }
 
-  // const { files } = commit.data
-  // const found = files.find(({ filename, status }) => (filename.toLowerCase() === CHANGELOG && (VALID_STATUS.includes(status))))
-  // const result = found ? SUCCESS : NEUTRAL
   const result = processCommit({ config: cfg, commit })
 
   return context.github.checks.create(context.repo({
@@ -52,7 +52,7 @@ async function check (context) {
     conclusion: result,
     completed_at: new Date(),
     output: {
-      title: 'Chaas check',
+      title: 'Chaas Check',
       summary: SUMMARY[result],
       text: MESSAGE[result]
     }
@@ -64,14 +64,13 @@ function processCommit ({ commit, config: { ignore } }) {
 
   const filtered = multimatch(
     files.map(f => f.filename),
-    ['*', ...ignore.map(i => `!${i}`)]
+    ['**', ...ignore.map(i => `!${i}`)]
   )
 
   if (!filtered.length) return NEUTRAL
 
   const hasChangelog = files.find(({ filename }) => filename.toLowerCase() === CHANGELOG)
-
-  return hasChangelog && VALID_STATUS.includes(hasChangelog.status) ? SUCCESS : NEUTRAL
+  return hasChangelog && VALID_STATUS.includes(hasChangelog.status) ? SUCCESS : FAILED
 }
 
 // Exports
