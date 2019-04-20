aecb3b96c0b0988378fca2001b9df75cd6fdf1b1
Max Fierro <elmasse@gmail.com>
2019-04-20 16:35:45 -0300

    Fixed: call toLamdba in probot-serverless-now.

--- handler.js
+++ handler.js
@@ -1,4 +1,4 @@
 
-const serverless = require('probot-serverless-now')
+const { toLambda } = require('probot-serverless-now')
 const appFn = require('./src/index')
-module.exports = serverless(appFn)
+module.exports = toLambda(appFn)
